3fe10271d8a3fc6385ad8027fda31f076108d073
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-11-21 13:37:23 +0200

    Add simple controllers

--- src/app/controllers/home.js
+++ src/app/controllers/home.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../data')
+
+const pageTitle = 'Project Puppy'
+
+// for rout specifid middleware
+// router.use((req, res, next) => {
+//   console.log('LOGGER')
+//   next()
+// })
 
 router.get('/', (req, res) => {
-  res.render('home')
+  db.Users.insertUser('someUsername')
+    .then(user => {
+      res.render('base-layout', { pageTitle })
+    })
 })
 
 module.exports = router
--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/home', require('./home'))
+router.use('/register', require('./register'))
 
 module.exports = router

2a801cee545ecc39c8418e7740c272185385d4bf
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-11-21 13:36:13 +0200

    Update app.js to support body-parser and serve public resources

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const path = require('path')
+
 const app = express()
+
+// mongoose setup
+mongoose.Promise = global.Promise // prevents the "depecated promise" message
+mongoose.connect('mongodb://localhost/test')
+mongoose.connection
+  .on('error', console.error.bind(console, 'connection error:'))
+  .once('open', () => console.log('DB connection successful!'))
 
 // view engine setup
 app.set('views', './app/views')
 app.set('view engine', 'pug')
+
+// middleware
+// app.use((req, res, next) => {
+//   console.log('Woohoo')
+//   next()
+// })
+
+// serve public resources
+app.use(express.static(path.join(__dirname, '/public')))
+
+// body-parser setup
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 // load controllers
 app.use(require('./app/controllers'))

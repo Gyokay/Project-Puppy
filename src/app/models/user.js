5f8127a5aec09ab710c9dd4e9a55ae3378a1d89d
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-11-21 23:49:01 +0200

    User model unique elements support

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -4,6 +4,18 @@ const Schema = mongoose.Schema
 let userSchema = new Schema({
   username: {
     type: String,
+    required: true,
+    unique: true,
+    dropDups: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    dropDups: true
+  },
+  password: {
+    type: String,
     required: true
   }
 })

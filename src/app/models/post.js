76b52a3df53b39a2807258fae8942b075d88834f
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-11-22 22:00:05 +0200

    Add Post model

---
+++ src/app/models/post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+let postSchema = new Schema({
+  ownerUsername: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  date: {
+    type: Date,
+    require: true
+  },
+  town: {
+    type: String,
+    require: true
+  },
+  petType: {
+    type: String,
+    enum: ['cat', 'dog', 'other'],
+    require: true
+  }
+})
+
+let Post = mongoose.model('Post', postSchema)
+
+module.exports = Post

2ff6efbd2c72ecfc9c63b4882ead1f8d8fc12cea
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-11-22 22:06:56 +0200

    Angry Pug

---
+++ src/public/js/register.js
@@ -0,0 +1,22 @@
+$(() => {
+  $('.submit').click(() => {
+    let username = $('#usernameInput').val()
+    let email = $('#emailInput').val()
+    let password = $('#passwordInput').val()
+
+    $.ajax({
+      url: '/register',
+      method: 'POST',
+      data: {
+        username,
+        email,
+        password
+      },
+      // dataType: 'application/json',
+      success: function (res) {
+        console.log(res)
+        window.location = res.redirect
+      }
+    })
+  })
+})

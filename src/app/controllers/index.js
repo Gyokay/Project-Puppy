71500fb6765608b0c6b44e65648e433c106829ce
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-11-22 22:02:37 +0200

    Add user controller

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -4,5 +4,6 @@ const router = express.Router()
 router.use('/home', require('./home'))
 router.use('/register', require('./register'))
 router.use('/login', require('./login'))
+router.use('/user', require('./user'))
 
 module.exports = router
---
+++ src/app/controllers/user.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../data')
+
+router.get('/:username', (req, res) => {
+  db.Users.getUserByUsername(req.params.username)
+    .then(user => {
+      if (!user) {
+        res.render('user-profile', { noUser: true })
+      }
+
+      res.render('user-profile', { username: user.username })
+    })
+})
+
+module.exports = router

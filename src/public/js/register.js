5bc8d249615ecad891629f909e0cadc00c9f93b2
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-11-23 11:37:18 +0200

    Add create post view

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -1,22 +1,24 @@
-$(() => {
-  $('.submit').click(() => {
-    let username = $('#usernameInput').val()
-    let email = $('#emailInput').val()
-    let password = $('#passwordInput').val()
+$
 
-    $.ajax({
-      url: '/register',
-      method: 'POST',
-      data: {
-        username,
-        email,
-        password
-      },
-      // dataType: 'application/json',
-      success: function (res) {
-        console.log(res)
-        window.location = res.redirect
-      }
-    })
-  })
-})
+// $(() => {
+//   $('.submit').click(() => {
+//     let username = $('#usernameInput').val()
+//     let email = $('#emailInput').val()
+//     let password = $('#passwordInput').val()
+
+//     $.ajax({
+//       url: '/register',
+//       method: 'POST',
+//       data: {
+//         username,
+//         email,
+//         password
+//       },
+//       // dataType: 'application/json',
+//       success: function (res) {
+//         console.log(res)
+//         window.location = res.redirect
+//       }
+//     })
+//   })
+// })

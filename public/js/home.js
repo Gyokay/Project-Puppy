f54234402a1a520521008c43ba2347b7eca758ad
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-12-03 18:03:13 +0200

    Redesign new messages display

--- public/js/home.js
+++ public/js/home.js
@@ -3,6 +3,18 @@ $(function () {
 
   socket.on('new messages count', function (count) {
     // console.log(count)
-    $('.msgCount').text(count)
+    let $msgCount = $('.msgCount')
+    let $chatLink = $('.chatLink')
+    $msgCount.text(count)
+    if (count > 0) {
+      $msgCount.addClass('red')
+
+      if (count === 1) {
+        $chatLink.text('message')
+      }
+    } else {
+      $msgCount.removeClass('red')
+      $chatLink.text('messages')
+    }
   })
 })

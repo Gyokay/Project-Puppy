ce3f1993b454abd12de347dabdecedc86994ef71
TalicZealot <talic.zealot@gmail.com>
2016-12-03 22:36:33 +0200

    added comments to posts

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -10,149 +10,166 @@ const request = require('request');
 const db = require('../data');
 
 router.get('/create-post', (req, res) => {
-  if (!req.isAuthenticated()) {
-    res.redirect('/login');
-    return;
-  }
+    if (!req.isAuthenticated()) {
+        res.redirect('/login');
+        return;
+    }
 
-  res.render('create-post', {
-    success: req.session.success,
-    errors: req.session.errors,
-    title: req.session.title,
-    description: req.session.description
-  });
+    res.render('create-post', {
+        success: req.session.success,
+        errors: req.session.errors,
+        title: req.session.title,
+        description: req.session.description
+    });
 
-  req.session.title = null
-  req.session.description = null
-  req.session.errors = []
-  req.session.save()
+    req.session.title = null
+    req.session.description = null
+    req.session.errors = []
+    req.session.save()
 });
 
 router.post('/create-post',
-  upload.array(constants.fileInputName, constants.allowedImagesCount),
-  (req, res) => {
-    if (!req.isAuthenticated()) {
-      res.redirect('/login');
-      return;
-    }
+    upload.array(constants.fileInputName, constants.allowedImagesCount),
+    (req, res) => {
+        if (!req.isAuthenticated()) {
+            res.redirect('/login');
+            return;
+        }
 
-    // Validation
-    req.checkBody('title', 'Title must be between ' + constants.minPostTitleLenght + ' and ' + constants.maxPostTitleLenght + ' characters')
-      .len(constants.minPostTitleLenght, constants.maxPostTitleLenght);
-    req.checkBody('description', 'Description must be between ' + constants.minPostDescriptionLenght + ' and ' + constants.maxPostDescriptionLenght + ' characters')
-      .len(constants.minPostDescriptionLenght, constants.maxPostDescriptionLenght);
+        // Validation
+        req.checkBody('title', 'Title must be between ' + constants.minPostTitleLenght + ' and ' + constants.maxPostTitleLenght + ' characters')
+            .len(constants.minPostTitleLenght, constants.maxPostTitleLenght);
+        req.checkBody('description', 'Description must be between ' + constants.minPostDescriptionLenght + ' and ' + constants.maxPostDescriptionLenght + ' characters')
+            .len(constants.minPostDescriptionLenght, constants.maxPostDescriptionLenght);
 
-    let errors = req.validationErrors();
+        let errors = req.validationErrors();
 
-    if (errors) {
-      req.session.errors = errors;
-      req.session.success = false;
-      req.session.title = req.body.title;
-      req.session.description = req.body.description;
-      res.redirect('/post/create-post');
-      return;
-    }
+        if (errors) {
+            req.session.errors = errors;
+            req.session.success = false;
+            req.session.title = req.body.title;
+            req.session.description = req.body.description;
+            res.redirect('/post/create-post');
+            return;
+        }
 
-    function insertPostToDB() {
-      // add new post to DB
-      db.Post.insertPost(
-        req.user.username,
-        req.body.title,
-        req.body.description,
-        req.body.town,
-        req.body.petType,
-        imgUrls
-      ).then(post => {
-        res.redirect(`/post/${post._id}`);
-      });
-    }
+        function insertPostToDB() {
+            // add new post to DB
+            db.Post.insertPost(
+                req.user.username,
+                req.body.title,
+                req.body.description,
+                req.body.town,
+                req.body.petType,
+                imgUrls
+            ).then(post => {
+                res.redirect(`/post/${post._id}`);
+            });
+        }
 
-    let imgUrls = [];
+        let imgUrls = [];
 
-    let filesCount = req.files.length;
-    let asyncCount = 0;
+        let filesCount = req.files.length;
+        let asyncCount = 0;
 
-    // options for the requester
-    // contains IMGUR api url and Client-ID
-    let options = {
-      method: 'POST',
-      url: 'https://api.imgur.com/3/upload',
-      headers: {
-        Authorization: 'Client-ID 9e21e0b5881553f'
-      }
-    }
-
-    // can be extracted to another file
-    // upload images to third party
-    new Promise((resolve, reject) => {
-      // Check if photos are available in the request body
-      if (filesCount <= 0) {
-        insertPostToDB();
-        return;
-      }
-
-      req.files.forEach(file => {
-        fs.createReadStream(file.path)
-          .pipe(request(options, (err, response, body) => {
-            if (err) {
-              console.log(err);
-              res.redirect('/post/create-post', { error: constants.uploadImgsErrorMsg });
-              return;
+        // options for the requester
+        // contains IMGUR api url and Client-ID
+        let options = {
+            method: 'POST',
+            url: 'https://api.imgur.com/3/upload',
+            headers: {
+                Authorization: 'Client-ID 9e21e0b5881553f'
             }
+        }
 
-            if (!err && response.statusCode === 200) {
-              let bodyObj = JSON.parse(body);
-              let imgLink = bodyObj.data.link;
+        // can be extracted to another file
+        // upload images to third party
+        new Promise((resolve, reject) => {
+                // Check if photos are available in the request body
+                if (filesCount <= 0) {
+                    insertPostToDB();
+                    return;
+                }
 
-              imgUrls.push(imgLink);
-              asyncCount++;
+                req.files.forEach(file => {
+                    fs.createReadStream(file.path)
+                        .pipe(request(options, (err, response, body) => {
+                            if (err) {
+                                console.log(err);
+                                res.redirect('/post/create-post', { error: constants.uploadImgsErrorMsg });
+                                return;
+                            }
 
-              if (asyncCount === filesCount) {
-                resolve();
-              }
-            }
-          }));
-      });
-    })
-      .then(() => {
-        // console.log(imgUrls)
-        // delete images from the file system
-        req.files.forEach(file => {
-          fs.unlink(file.path);
-        });
+                            if (!err && response.statusCode === 200) {
+                                let bodyObj = JSON.parse(body);
+                                let imgLink = bodyObj.data.link;
 
-        insertPostToDB();
-      });
-  });
+                                imgUrls.push(imgLink);
+                                asyncCount++;
+
+                                if (asyncCount === filesCount) {
+                                    resolve();
+                                }
+                            }
+                        }));
+                });
+            })
+            .then(() => {
+                // console.log(imgUrls)
+                // delete images from the file system
+                req.files.forEach(file => {
+                    fs.unlink(file.path);
+                });
+
+                insertPostToDB();
+            });
+    });
 
 router.get('/:postId', (req, res) => {
-  db.Post.getPostById(req.params.postId)
-    .then(post => {
-      if (!post) {
-        res.render('not-found');
-        return;
-      }
+        db.Post.getPostById(req.params.postId)
+            .then(post => {
+                if (!post) {
+                    res.render('not-found');
+                    return;
+                }
 
-      let isOwner = false
+                let isOwner = false
 
-      if (req.isAuthenticated()) {
-        if (req.user.username === post.ownerUsername) {
-          isOwner = true
-        }
-      }
+                if (req.isAuthenticated()) {
+                    if (req.user.username === post.ownerUsername) {
+                        isOwner = true
+                    }
+                }
 
-      res.render('view-post', {
-        title: post.title,
-        description: post.description,
-        date: post.creationDate.toDateString(),
-        ownerUsername: post.ownerUsername,
-        town: post.town,
-        petType: post.petType,
-        imgUrls: post.imgUrls,
-        isArchived: post.isArchived,
-        isOwner
-      });
+                res.render('view-post', {
+                    title: post.title,
+                    description: post.description,
+                    date: post.creationDate.toDateString(),
+                    ownerUsername: post.ownerUsername,
+                    town: post.town,
+                    petType: post.petType,
+                    imgUrls: post.imgUrls,
+                    isArchived: post.isArchived,
+                    messages: post.messages,
+                    isOwner: post.isOwner,
+                    _id: post._id
+                });
+            });
+    })
+    .post('/:postId', (req, res) => {
+        let id = req.params.postId;
+        let user = req.user.username;
+        let content = req.body;
+        db.Post.addMessageToPostById(
+                id, {
+                    user,
+                    content,
+                    date: new Date()
+                }
+            )
+            .then((post) => {
+                res.redirect(`/post/${post._id}`);
+            })
     });
-});
 
 module.exports = router;
--- app/data/post.js
+++ app/data/post.js
@@ -1,136 +1,152 @@
-module.exports = function (options) {
-  options = options || {}
+const Post = require('../models/post');
 
-  if (!options.Post) {
-    options.Post = require('../models/post')
-  }
+module.exports = function(options) {
+    options = options || {}
 
-  return {
-    insertPost(ownerUsername, title, description, town, petType, imgUrls) {
-      return new Promise((resolve, reject) => {
-        let newPost = new options.Post({
-          ownerUsername,
-          title,
-          description,
-          town,
-          petType,
-          imgUrls,
-          creationDate: new Date(),
-          isArchived: false
-        })
-
-        newPost.save((err, post) => {
-          if (err) {
-            console.log(err)
-          }
-          resolve(post)
-        })
-      })
-    },
-
-    updateIsArchivedById(_id, isArchived) {
-      return new Promise((resolve, reject) => {
-        options.Post.update({ _id }, { $set: { isArchived } }, (err, post) => {
-          if (err) {
-            console.log(err)
-          }
-          resolve(post)
-        })
-      })
-    },
-
-    getPostById(_id) {
-      return new Promise((resolve, reject) => {
-        // Regular expression that checks for hex value
-        var checkForHexRegExp = new RegExp('^[0-9a-fA-F]{24}$')
-        let isValidId = checkForHexRegExp.test(_id)
-
-        if (!isValidId) {
-          return resolve(null)
-        }
-
-        options.Post.findOne({ _id }, (err, post) => {
-          if (err) {
-            console.log(err)
-          }
-          resolve(post)
-        })
-      })
-    },
-
-    getLatest(count, excludedPostIds) {
-      return new Promise((resolve, reject) => {
-        options.Post.find({
-          // creationDate: { $lt: new Date() },
-          _id: { $nin: excludedPostIds },
-          isArchived: false
-        })
-          .sort({ creationDate: 'desc' })
-          .limit(count)
-          .exec((err, posts) => {
-            if (err) {
-              console.log(err)
-            }
-            resolve(posts)
-          })
-      })
-    },
-
-    getByTown(town, count, excludedPostIds) {
-      return new Promise((resolve, reject) => {
-        options.Post.find({
-          town,
-          _id: { $nin: excludedPostIds },
-          isArchived: false
-        })
-          .sort({ creationDate: 'desc' })
-          .limit(count)
-          .exec((err, posts) => {
-            if (err) {
-              console.log(err)
-            }
-            resolve(posts)
-          })
-      })
-    },
-
-    getByPetType(petType, count, excludedPostIds) {
-      return new Promise((resolve, reject) => {
-        options.Post.find({
-          petType,
-          _id: { $nin: excludedPostIds },
-          isArchived: false
-        })
-          .sort({ creationDate: 'desc' })
-          .limit(count)
-          .exec((err, posts) => {
-            if (err) {
-              console.log(err)
-            }
-            resolve(posts)
-          })
-      })
-    },
-
-    getByTownAndPetType(town, petType, count, excludedPostIds) {
-      return new Promise((resolve, reject) => {
-        options.Post.find({
-          town,
-          petType,
-          _id: { $nin: excludedPostIds },
-          isArchived: false
-        })
-          .sort({ creationDate: 'desc' })
-          .limit(count)
-          .exec((err, posts) => {
-            if (err) {
-              console.log(err)
-            }
-            resolve(posts)
-          })
-      })
+    if (!options.Post) {
+        options.Post = require('../models/post')
     }
-  }
+
+    return {
+        insertPost(ownerUsername, title, description, town, petType, imgUrls) {
+            return new Promise((resolve, reject) => {
+                let newPost = new options.Post({
+                    ownerUsername,
+                    title,
+                    description,
+                    town,
+                    petType,
+                    imgUrls,
+                    creationDate: new Date(),
+                    isArchived: false
+                })
+
+                newPost.save((err, post) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    resolve(post)
+                })
+            })
+        },
+
+        updateIsArchivedById(_id, isArchived) {
+            return new Promise((resolve, reject) => {
+                options.Post.update({ _id }, { $set: { isArchived } }, (err, post) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    resolve(post)
+                })
+            })
+        },
+
+        getPostById(_id) {
+            return new Promise((resolve, reject) => {
+                // Regular expression that checks for hex value
+                var checkForHexRegExp = new RegExp('^[0-9a-fA-F]{24}$')
+                let isValidId = checkForHexRegExp.test(_id)
+
+                if (!isValidId) {
+                    return resolve(null)
+                }
+
+                options.Post.findOne({ _id }, (err, post) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    resolve(post)
+                })
+            })
+        },
+
+        getLatest(count, excludedPostIds) {
+            return new Promise((resolve, reject) => {
+                options.Post.find({
+                        // creationDate: { $lt: new Date() },
+                        _id: { $nin: excludedPostIds },
+                        isArchived: false
+                    })
+                    .sort({ creationDate: 'desc' })
+                    .limit(count)
+                    .exec((err, posts) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        resolve(posts)
+                    })
+            })
+        },
+
+        getByTown(town, count, excludedPostIds) {
+            return new Promise((resolve, reject) => {
+                options.Post.find({
+                        town,
+                        _id: { $nin: excludedPostIds },
+                        isArchived: false
+                    })
+                    .sort({ creationDate: 'desc' })
+                    .limit(count)
+                    .exec((err, posts) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        resolve(posts)
+                    })
+            })
+        },
+
+        getByPetType(petType, count, excludedPostIds) {
+            return new Promise((resolve, reject) => {
+                options.Post.find({
+                        petType,
+                        _id: { $nin: excludedPostIds },
+                        isArchived: false
+                    })
+                    .sort({ creationDate: 'desc' })
+                    .limit(count)
+                    .exec((err, posts) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        resolve(posts)
+                    })
+            })
+        },
+        addMessageToPostById(id, { user, content, date }) {
+            return new Promise((resolve, reject) => {
+                Post.findOne({ _id: id }, (err, post) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    post.messages.push({ user, content, date });
+                    return post.save(post)
+                        .then(resolve)
+                        .catch(reject);
+                })
+            })
+        },
+
+        getByTownAndPetType(town, petType, count, excludedPostIds) {
+            return new Promise((resolve, reject) => {
+                options.Post.find({
+                        town,
+                        petType,
+                        _id: { $nin: excludedPostIds },
+                        isArchived: false
+                    })
+                    .sort({ creationDate: 'desc' })
+                    .limit(count)
+                    .exec((err, posts) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        resolve(posts)
+                    })
+            })
+        }
+    }
 }
 
 // const Post = require('../models/post')

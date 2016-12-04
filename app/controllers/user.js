b422fc0a0801872fadd05aa510a96dff3df147a9
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-12-04 15:46:32 +0200

    Merge

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,55 +3,31 @@ const router = express.Router()
 const db = require('../data')
 
 router.get('/:username', (req, res) => {
-<<<<<<< HEAD
-  db.Users.getUserByUsername(req.params.username)
-    .then(user => {
-      if (!user) {
-        res.render('not-found')
-        return
-      }
+  db.Post.getByUsername(req.params.username)
+    .then(posts => {
+      db.Users.getUserByUsername(req.params.username)
+        .then(user => {
+          if (!user) {
+            res.render('not-found')
+            return
+          }
 
-      let isOwner = false
+          let isOwner = false
 
-      if (req.isAuthenticated()) {
-        if (req.user.username === req.params.username) {
-          isOwner = true
-        }
-      }
+          if (req.isAuthenticated()) {
+            if (req.user.username === req.params.username) {
+              isOwner = true
+            }
+          }
 
-      res.render('user-profile', {
-        username: user.username,
-        email: user.email,
-        isOwner
-      })
-    })
-=======
-    db.Post.getByUsername(req.params.username)
-        .then(posts => {
-            db.Users.getUserByUsername(req.params.username)
-                .then(user => {
-                    if (!user) {
-                        res.render('not-found')
-                        return
-                    }
-
-                    let isOwner = false
-
-                    if (req.isAuthenticated()) {
-                        if (req.user.username === req.params.username) {
-                            isOwner = true
-                        }
-                    }
-
-                    res.render('user-profile', {
-                        username: user.username,
-                        email: user.email,
-                        posts,
-                        isOwner
-                    })
-                })
+          res.render('user-profile', {
+            username: user.username,
+            email: user.email,
+            posts,
+            isOwner
+          })
         })
->>>>>>> 8a8da59ab90c7fc3a6059c06ec379ba2f3615369
+    })
 })
 
 module.exports = router

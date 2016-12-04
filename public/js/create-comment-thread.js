990a95708ac80e0769d4c79b6cb658f20fba6b12
TalicZealot <talic.zealot@gmail.com>
2016-12-04 23:06:55 +0200

    validation fixes

--- public/js/create-comment-thread.js
+++ public/js/create-comment-thread.js
@@ -1,12 +1,12 @@
-$(function () {
+$(function() {
     const minCommentLength = 1,
-        maxCommentLength = 300,       
+        maxCommentLength = 300,
         msgError = 'The {0} must be between {1} and {2} characters long!',
         errorHtmlTag = '<p class=\'error alert\'>{0}</p>';
 
     let errors = [];
 
-    $('#submitComment').submit(function (e){
+    $('#submitComment').submit(function(e) {
         // Get value
         let comment = $('#commentInput').val();
 
@@ -15,12 +15,12 @@ $(function () {
 
         // Validate on submit
         validateLength(minCommentLength, maxCommentLength, comment.length, 'Comment');
-        
+
         // Show errors
         if (Array.from(errors).length > 0) {
             e.preventDefault();
 
-            errors.forEach(function (error) {
+            errors.forEach(function(error) {
                 $('.pageTitle').after(stringFormat(errorHtmlTag, [error]));
             });
 
@@ -29,14 +29,14 @@ $(function () {
     });
 
     // Helper functions
-    function validateLength (minLen, maxLen, val, elementName){
-        if((maxLen < val) || (val < minLen)){
+    function validateLength(minLen, maxLen, val, elementName) {
+        if ((maxLen < val) || (val < minLen)) {
             errors.push(stringFormat(msgError, [elementName, minLen, maxLen]));
         }
     }
 
     function stringFormat(source, params) {
-        $.each(params,function (i, n) {
+        $.each(params, function(i, n) {
             source = source.replace(new RegExp("\\{" + i + "\\}", "g"), n);
         });
         return source;
